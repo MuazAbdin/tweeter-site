fccfcc616526ea1032eb760e7dfe15d3697d1f64
Muaz Abdin <muathabd25@gmail.com>
2023-11-26 20:52:52 +0200

    create the outline of the Tweeter model

---
+++ model.js
@@ -0,0 +1,18 @@
+const Tweeter = function () {
+  /* attributes */
+  const _posts = [];
+
+  let postIdCounter = 0;
+  let commentIdCounter = 0;
+
+  /* Private Methods */
+
+  /* Public Methods */
+  function getPosts() {}
+
+  function addPost(text) {}
+
+  function removePost(postID) {}
+
+  return { getPosts, addPost, removePost };
+};
