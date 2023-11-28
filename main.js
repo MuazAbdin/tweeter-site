b6830d468d7932fc546d7d42bedfcb46f49ebe63
Muaz Abdin <muathabd25@gmail.com>
2023-11-28 13:01:44 +0200

    add listener to add comment  button

--- main.js
+++ main.js
@@ -4,16 +4,25 @@ const renderer = Renderer();
 /* EventListeners */
 // TWIT button
 $("#post-btn").click(function () {
-  const text = $(this).prev();
-  // console.log(text.val());
-  tweeter.addPost(text.val());
-  text.val("");
+  const input = $(this).prev();
+  // console.log(input.val());
+  tweeter.addPost(input.val());
+  input.val("");
   renderer.renderPosts(tweeter.getPosts());
 });
 
 // Delete Post button
 $(".all-posts").on("click", ".post-controller .del-btn", function () {
   tweeter.removePost($(this).closest(".post").data().id);
+  renderer.renderPosts(tweeter.getPosts());
+});
+
+// Add Comment button
+$(".all-posts").on("click", ".comment-btn", function () {
+  const postID = $(this).closest(".post").data().id;
+  const input = $(this).prev();
+  tweeter.addComment(input.val(), postID);
+  input.val("");
   renderer.renderPosts(tweeter.getPosts());
 });
 
